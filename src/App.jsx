071b17b80e728e937bb84fc51cf5bf4812a050a8
Matt <matthew.t.kissinger@gmail.com>
2023-02-27 10:07:42 -0800

    refactored with 2 states in App.jsx - 1 for taskInput - 1 for tasksArray

--- src/App.jsx
+++ src/App.jsx
@@ -5,140 +5,88 @@ import './App.css'
 import Overview from './components/Overview'
 
 // TODO 
-// style with bootcamp for learning purposes
+// come back for HARD challenge later
+
 
 function App() {
-
-  const [tasksArray, setTasksArray] = React.useState(
+  // set up state for input field
+  const [taskInput, setTaskInput] = React.useState(
     {
-      task: {
-        text: '', 
-        id: uniqid(),
-        editMode: false
-      },
-      tasks: []
-    } 
-  );
+      text: '',
+      id: uniqid(),
+      editMode: false
+    }
+  )
+
+  // set up state for taskArray storage
+  const [tasksArray, setTasksArray] = React.useState([]);
 
   function handleChange(e) {
-    setTasksArray(prevState => {
-      return ({
-        ...prevState,
-        task: {
+    setTaskInput(prevState => {
+      return (
+        {
           text: e.target.value,
-          id: prevState.task.id,
+          id: prevState.id,
           editMode: false
         }
-      })
+      )
     })
   }
 
   function addNewTask() {
     setTasksArray(prevState => {
       return (
-        {
-          tasks: [...prevState.tasks,
+        [...prevState,
             {
-              text: prevState.task.text,
-              id: prevState.task.id,
+              text: taskInput.text,
+              id: taskInput.id,
               editMode: false
             } 
-          ],
-          task: {
-            text: '',
-            id: uniqid(),
-            editMode: false
-          }
-        }
+        ]
       )
+    })
+
+    setTaskInput({
+      text: '',
+      id: uniqid(),
+      editMode: false
     })
   }
 
-  function deleteTask(e) {
-    const title = e.target.id;
+  function deleteTask(id) {
+
+    const newArray = tasksArray.filter(task => task.id !== id);
     
-    setTasksArray(prevState => {
-      return {
-          task: {
-            text: '', 
-            id: uniqid(),
-            editMode: false
-          },
-          tasks: prevState.tasks.filter(task => task.text !== title) 
-      }
-      }  
+    setTasksArray(newArray);
+  }
+
+  const tasks = tasksArray.map(task => {
+    return (
+      <Overview
+        key={task.id}
+        id={task.id}
+        tasksArray={tasksArray}
+        taskInput={taskInput}
+        taskData={task} 
+        onDeleteBtnClick={() => deleteTask(task.id)}
+      />
     )
-  }
+  })
 
-  function editTaskMode(e) {
-    const targetId = e.target.id;
-    const targetIndex = tasksArray.tasks.findIndex(task => task.id === targetId);
-
-    function updateEditModeTrue(targetIndex) {
-      const newArray = tasksArray.tasks.map((task, i) => {
-        if (targetIndex === i) {
-          return {...task, editMode: true};
-        } else {
-          return task;
-        }
-      })
-      return newArray;
-    }
-
-    const newArray = updateEditModeTrue(targetIndex);
-
-    setTasksArray(
-      {
-        task: {
-          text: '', 
-          id: uniqid(),
-          editMode: false
-        },
-        tasks: newArray
-      }
-    )   
-  }
-
-  function submitEdit(e) {
-    console.log('submit changes to task text')
-    // const targetIndex = tasksArray.tasks.findIndex(task => task.id === e.target.id);
-    
-    // function updateTaskText(index) {
-    //   const newArray = tasksArray.tasks.map((task, i) => {
-    //     if (targetIndex === i) {
-    //       return {...task, 
-    //             text: '',
-    //             editMode: false};
-    //     } else {
-    //       return task;
-    //     }
-    //   })
-    //   return newArray;
-    // }
-  }
-
-  function editFieldTextChange(e) {
-    console.log(e.target.id)
-  }
 
   return (
     <div className="App--container">
       <input 
         className='newTaskName' 
         type="text" 
-        value={tasksArray.task.text}
+        value={taskInput.text}
         onChange={handleChange}>
       </input>
       <button onClick={addNewTask}
         className='add-task-btn'>
         Add Task
       </button>
-      <Overview 
-        tasksArray={tasksArray} 
-        onDeleteBtnClick={deleteTask}
-        onEditBtnClick={editTaskMode}
-        onTextChange={editFieldTextChange}
-        onClickReSubmit={submitEdit}/>
+      {tasks}
     </div>
   )
 }
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -2,61 +2,24 @@
 import React from 'react'
 
 export default function Overview(props) {
-    const tasksArray = props.tasksArray.tasks;
 
-    const tasks = tasksArray.map(task => {
-        console.log(task.editMode)
-        if (task.editMode === false) {
+    const { taskData, tasksArray } = props;
+
             return (
-                <div key={task.text} className='task--container'>
-                    <p key={task.id}>
-                        {`${tasksArray.indexOf(task) + 1}.`} {task.text}
+                <div className='task--container'>
+                    <p>
+                        {`${tasksArray.indexOf(taskData) + 1}.`} {taskData.text}
                     </p>
                     <button 
                         onClick={props.onEditBtnClick}
-                        id={task.id}
                         className="task--edit-btn">
                             Edit
                     </button>
                     <button 
                         onClick={props.onDeleteBtnClick}
-                        id={task.text}
                         className="task--delete-btn">
                         delete
                     </button>
                 </div>   
             )
-        } else {
-            return (
-                <div key={task.text} className='task--container'>
-                <input 
-                    key={task.id} 
-                    id={task.id}
-                    value={task.text}
-                    onChange={props.onTextChange}
-                    className="task--input-edit">
-                </input>
-                <button 
-                    onClick={props.onClickReSubmit}
-                    id={task.id}
-                    className="task--edit-btn">
-                        re-submit
-                </button>
-                <button 
-                    onClick={props.onDeleteBtnClick}
-                    id={task.text}
-                    className="task--delete-btn">
-                    delete
-                </button>
-            </div> 
-            )
-        }
-        
-    });
-
-    return (
-        <div>
-            {tasks}
-        </div>
-    )
 }
